c03044ff9164b6e47c97b410bfd11b4ba3bffddd
Schouten Robin <srobin@w-r8-p7.s19.be>
2019-04-24 19:32:58 +0200

    Added typedef struct s_tetra

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: qbackaer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 17:46:07 by qbackaer          #+#    #+#             */
-/*   Updated: 2019/04/24 17:47:45 by qbackaer         ###   ########.fr       */
+/*   Updated: 2019/04/24 19:09:43 by srobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,13 @@
 # include <string.h>
 # include <fcntl.h>
 # include "libft/libft.h"
+
+# define BUFF_SIZE 21
+
+typedef struct		s_tetra
+{
+	int		x[4];
+	int		y[4];
+}					t_tetra;
 
 #endif
